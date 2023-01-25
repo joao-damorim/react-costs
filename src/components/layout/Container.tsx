4f130121d61a369ec6b9788619964291ae4dcc28
João D'Amorim <joao.damorim@gmail.com>
2023-01-25 11:05:44 -0300

    Structuring the project

---
+++ src/components/layout/Container.tsx
@@ -0,0 +1,19 @@
+import styles from './Container.module.css'
+import { ReactNode } from 'react'
+
+
+interface IContainer {
+    customClass: any,
+    children: ReactNode
+}
+
+function Container(props: IContainer) {
+    return (
+        <div className={`${styles.container} ${styles[props.customClass]}`}>
+            {props.children}
+        </div>
+
+    )
+}
+
+export default Container
