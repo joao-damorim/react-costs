c666144e9def268c7336ebf058a5ac3888685a53
João D'Amorim <joao.damorim@gmail.com>
2023-01-25 15:28:46 -0300

    Project form page

---
+++ src/components/project/ProjecForm.tsx
@@ -0,0 +1,18 @@
+function ProjectForm() {
+    return (
+        <form>
+            <div>
+                <input type="text" placeholder="Insira o nome do projeto" />
+                <input type="number" placeholder="Insira o orçamento total" />
+                <select name="category_id">
+                    <option disabled>Selecione a categoria</option>
+                </select>
+            </div>
+            <div>
+                <input type="submit" value="Criar projeto" />
+            </div>
+        </form>
+    )
+}
+
+export default ProjectForm
