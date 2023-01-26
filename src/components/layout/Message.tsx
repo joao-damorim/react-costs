bbedf1fed23dc1d33c511e2b5c5509a426db953f
João D'Amorim <joao.damorim@gmail.com>
2023-01-26 10:09:52 -0300

    System Messages - part 2 - useState and useEffect

--- src/components/layout/Message.tsx
+++ src/components/layout/Message.tsx
@@ -1,13 +1,37 @@
 import styles from './Message.module.css'
-
+import { useState, useEffect } from 'react'
+import { flattenDiagnosticMessageText } from 'typescript'
 interface IMessage {
     msg: string,
     type: string
 }
 
 function Message(props: IMessage) {
+    
+    const [visible, setVisible] = useState(false)
+    
+    useEffect(() => {
+        if(!props.msg) {
+            setVisible(false)
+            return
+        }
+
+        setVisible(true)
+
+        const timer = setTimeout(() => {
+            setVisible(false)
+        }, 3000)
+
+        return () => clearTimeout(timer)
+
+    }, [props.msg])
+
     return (
-        <div className={`${styles.message} ${styles[props.type]}`}>{props.msg}</div>
+        <>
+        {visible && (
+            <div className={`${styles.message} ${styles[props.type]}`}>{props.msg}</div>
+        )}
+        </>
     )
 }
 
